70eaeb5e617d066571045e8d1d54a8c18ee81546
Lyrapuff <lyrapuff@gmail.com>
2022-01-04 14:26:47 +0300

    uniforms and depth testing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 use std::sync::Arc;
-use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer, TypedBufferAccess};
+use std::time::Instant;
 
+use vulkano::pipeline::graphics::depth_stencil::DepthStencilState;
+use vulkano::descriptor_set::PersistentDescriptorSet;
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer, CpuBufferPool, TypedBufferAccess};
 use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, SubpassContents};
 use vulkano::device::physical::PhysicalDevice;
+use vulkano::format::Format;
 use vulkano::device::{Device, DeviceExtensions, Features};
 use vulkano::image::view::ImageView;
-use vulkano::image::{ImageAccess, SwapchainImage};
+use vulkano::image::{AttachmentImage, ImageAccess, SwapchainImage};
 use vulkano::instance::Instance;
 use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
 use vulkano::pipeline::graphics::vertex_input::BuffersDefinition;
 use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
-use vulkano::pipeline::GraphicsPipeline;
+use vulkano::pipeline::{GraphicsPipeline, Pipeline, PipelineBindPoint};
 use vulkano::render_pass::{Framebuffer, RenderPass, Subpass};
 use vulkano::swapchain::{self, AcquireError, Swapchain, SwapchainCreationError};
 use vulkano::sync::{self, FlushError, GpuFuture};
 use vulkano::Version;
 
 use vulkano_win::VkSurfaceBuild;
-use winit::event::{Event, WindowEvent};
 
+use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
+
+use nalgebra_glm::{identity, look_at, perspective, pi, rotate_normalized_axis, TMat4, translate, vec3};
 
 #[derive(Default, Debug, Clone)]
 struct Vertex {
@@ -29,7 +35,28 @@ struct Vertex {
 }
 vulkano::impl_vertex!(Vertex, position, color);
 
+#[derive(Debug, Clone)]
+struct MVP {
+    model: TMat4<f32>,
+    view: TMat4<f32>,
+    projection: TMat4<f32>
+}
+
+impl MVP {
+    fn new() -> MVP {
+        MVP {
+            model: identity(),
+            view: identity(),
+            projection: identity()
+        }
+    }
+}
+
 fn main() {
+    let mut mvp = MVP::new();
+    mvp.view = look_at(&vec3(0.0, 0.0, 0.01), &vec3(0.0, 0.0, 0.0), &vec3(0.0, -1.0, 0.0));
+    mvp.model = translate(&identity(), &vec3(0.0, 0.0, -0.5));
+
     let instance = {
         let extensions = vulkano_win::required_extensions();
         Instance::new(None, Version::V1_1, &extensions, None).unwrap()
@@ -62,6 +89,12 @@ fn main() {
         let alpha = caps.supported_composite_alpha.iter().next().unwrap();
         let format = caps.supported_formats[0].0;
         let dimensions: [u32; 2] = surface.window().inner_size().into();
+        mvp.projection = perspective(
+            dimensions[0] as f32 / dimensions[1] as f32,
+            180.0,
+            0.01,
+            100.0
+        );
 
         Swapchain::start(device.clone(), surface.clone())
             .num_images(caps.min_image_count)
@@ -82,11 +115,17 @@ fn main() {
                 store: Store,
                 format: swapchain.format(),
                 samples: 1,
+            },
+            depth: {
+                load: Clear,
+                store: DontCare,
+                format: Format::D16_UNORM,
+                samples: 1,
             }
         },
         pass: {
             color: [color],
-            depth_stencil: {}
+            depth_stencil: {depth}
         }
     ).unwrap();
 
@@ -100,8 +139,15 @@ layout(location = 1) in vec3 color;
 
 layout(location = 0) out vec3 out_color;
 
+layout(set = 0, binding = 0) uniform MVP_Data {
+    mat4 world;
+    mat4 view;
+    mat4 projection;
+} uniforms;
+
 void main() {
-    gl_Position = vec4(position, 1.0);
+    mat4 worldview = uniforms.view * uniforms.world;
+    gl_Position = uniforms.projection * worldview * vec4(position, 1.0);
     out_color = color;
 }"
         }
@@ -126,20 +172,27 @@ void main() {
     let vs = vs::load(device.clone()).unwrap();
     let fs = fs::load(device.clone()).unwrap();
 
+    let uniform_buffer = CpuBufferPool::<vs::ty::MVP_Data>::uniform_buffer(device.clone());
+
     let pipeline = GraphicsPipeline::start()
         .vertex_input_state(BuffersDefinition::new().vertex::<Vertex>())
         .vertex_shader(vs.entry_point("main").unwrap(), ())
         .input_assembly_state(InputAssemblyState::new())
         .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
         .fragment_shader(fs.entry_point("main").unwrap(), ())
+        .depth_stencil_state(DepthStencilState::simple_depth_test())
         .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
         .build(device.clone())
         .unwrap();
 
     let vertex_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, [
-        Vertex { position: [-0.5, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-        Vertex { position: [0.5, 0.5, 0.0], color: [0.0, 1.0, 0.0] },
-        Vertex { position: [0.0, -0.5, 0.0], color: [0.0, 0.0, 1.0] }
+        Vertex { position: [-0.5, 0.5, -0.5], color: [0.0, 0.0, 0.0] },
+        Vertex { position: [0.5, 0.5, -0.5], color: [0.0, 0.0, 0.0] },
+        Vertex { position: [0.0, -0.5, -0.5], color: [0.0, 0.0, 0.0] },
+
+        Vertex { position: [-0.5, -0.5, -0.6], color: [1.0, 1.0, 1.0] },
+        Vertex { position: [0.5, -0.5, -0.6], color: [1.0, 1.0, 1.0] },
+        Vertex { position: [0.0, 0.5, -0.6], color: [1.0, 1.0, 1.0] }
     ].iter().cloned()).unwrap();
 
     let mut viewport = Viewport {
@@ -148,11 +201,13 @@ void main() {
         depth_range: 0.0..1.0,
     };
 
-    let mut framebuffers = window_size_dependent_setup(&images, render_pass.clone(), &mut viewport);
+    let mut framebuffers = window_size_dependent_setup(device.clone(), &images, render_pass.clone(), &mut viewport);
 
     let mut recreate_swapchain = false;
 
     let mut previous_frame_end = Some(Box::new(sync::now(device.clone())) as Box<dyn GpuFuture>);
+
+    let rotation_start = Instant::now();
 
     event_loop.run(move |event, _, control_flow| {
         match event {
@@ -167,6 +222,13 @@ void main() {
 
                 if recreate_swapchain {
                     let dimensions: [u32; 2] = surface.window().inner_size().into();
+                    mvp.projection = perspective(
+                        dimensions[0] as f32 / dimensions[1] as f32,
+                        180.0,
+                        0.01,
+                        100.0
+                    );
+
                     let (new_swapchain, new_images) = match swapchain.recreate().dimensions(dimensions).build() {
                         Ok(r) => r,
                         Err(SwapchainCreationError::UnsupportedDimensions) => return,
@@ -174,10 +236,34 @@ void main() {
                     };
 
                     swapchain = new_swapchain;
-                    framebuffers = window_size_dependent_setup(&new_images, render_pass.clone(), &mut viewport);
+                    framebuffers = window_size_dependent_setup(device.clone(), &new_images, render_pass.clone(), &mut viewport);
 
                     recreate_swapchain = false;
                 }
+
+                let uniform_buffer_subbuffer = {
+                    let elapsed = rotation_start.elapsed().as_secs() as f64 + rotation_start.elapsed().subsec_nanos() as f64 / 1_000_000_000.0;
+                    let elapsed_as_radians = elapsed * pi::<f64>() / 180.0 * 30.0;
+                    let model = rotate_normalized_axis(
+                        &mvp.model,
+                        elapsed_as_radians as f32,
+                        &vec3(0.0, 0.0, 1.0)
+                    );
+
+                    let uniform_data = vs::ty::MVP_Data {
+                        //world: model.into(),
+                        world: (identity() as TMat4<f32>).into(),
+                        view: mvp.view.into(),
+                        projection: mvp.projection.into(),
+                    };
+
+                    uniform_buffer.next(uniform_data).unwrap()
+                };
+
+                let layout = pipeline.layout().descriptor_set_layouts().get(0).unwrap();
+                let mut set_builder = PersistentDescriptorSet::start(layout.clone());
+                set_builder.add_buffer(uniform_buffer_subbuffer).unwrap();
+                let set = set_builder.build().unwrap();
 
                 let (image_num, suboptimal, acquire_future) = match swapchain::acquire_next_image(swapchain.clone(), None) {
                     Ok(r) => r,
@@ -192,7 +278,7 @@ void main() {
                     recreate_swapchain = true;
                 }
 
-                let clear_value = vec!([0.0, 0.0, 0.0, 1.0].into());
+                let clear_value = vec!([0.8, 0.7, 0.85, 1.0].into(), 1f32.into());
 
                 let mut cmd_buffer_builder = AutoCommandBufferBuilder::primary(
                     device.clone(),
@@ -205,6 +291,12 @@ void main() {
                     .unwrap()
                     .set_viewport(0, [viewport.clone()])
                     .bind_pipeline_graphics(pipeline.clone())
+                    .bind_descriptor_sets(
+                        PipelineBindPoint::Graphics,
+                        pipeline.layout().clone(),
+                        0,
+                        set.clone(),
+                    )
                     .bind_vertex_buffers(0, vertex_buffer.clone())
                     .draw(vertex_buffer.len() as u32, 1, 0, 0)
                     .unwrap()
@@ -238,12 +330,18 @@ void main() {
 }
 
 fn window_size_dependent_setup(
+    device: Arc<Device>,
     images: &[Arc<SwapchainImage<Window>>],
     render_pass: Arc<RenderPass>,
     viewport: &mut Viewport,
 ) -> Vec<Arc<Framebuffer>> {
     let dimensions = images[0].dimensions().width_height();
+
     viewport.dimensions = [dimensions[0] as f32, dimensions[1] as f32];
+
+    let depth_buffer = ImageView::new(
+        AttachmentImage::transient(device.clone(), dimensions, Format::D16_UNORM).unwrap(),
+    ).unwrap();
 
     images
         .iter()
@@ -251,6 +349,8 @@ fn window_size_dependent_setup(
             let view = ImageView::new(image.clone()).unwrap();
             Framebuffer::start(render_pass.clone())
                 .add(view)
+                .unwrap()
+                .add(depth_buffer.clone())
                 .unwrap()
                 .build()
                 .unwrap()
